78cf330bb336fd82ef45a6b60c75bd7c77694097
PRAJNESH111 <prajneshpajju094@gmail.com>
2025-08-19 09:33:52 +0530

    first commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:8080/api/' });
+const API = axios.create({
+  baseURL:
+    "https://image-generation-0g9d.onrender.com/api/" ||
+    "http://localhost:8080/api/",
+});
 
 export const GetPosts = async () => await API.get("/post/");
 export const CreatePost = async (data) => await API.post("/post/", data);
-export const GenerateAIImage = async (data) => await API.post("/generateImage/", data); 
+export const GenerateAIImage = async (data) =>
+  await API.post("/generateImage/", data);
