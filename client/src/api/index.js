8222903cb8b7a6447eb6b9b07bf24f13a1cf1acb
Prajnesh kumar <96726375+PRAJNESH111@users.noreply.github.com>
2025-08-19 09:35:04 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8080/api/' });
+const API = axios.create({ baseURL:   baseURL:
+    "https://image-generation-0g9d.onrender.com/api/" ||
+    "http://localhost:8080/api/", });
 
 export const GetPosts = async () => await API.get("/post/");
 export const CreatePost = async (data) => await API.post("/post/", data);
